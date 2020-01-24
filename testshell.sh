82648b655dce940079bbd9da92decedd01c5ea25
Bram Daams <B.Daams@science.ru.nl>
2020-01-24 15:00:28 +0100

    test script for sch

---
+++ testshell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# crude way to list cron commands in /etc/cron.d/test
+grep -v ^# /etc/cron.d/test | grep . | sed 's/^.* root //' |
+	while read line
+	do
+		
+		echo "testing: sch -c $line" | boxes -d unicornthink
+		sch -c "$line"
+	done
