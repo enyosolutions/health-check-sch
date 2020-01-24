a536f32f4c88f7e64b8302a80a35403773a04c59
Bram Daams <b.daams@science.ru.nl>
2020-01-25 00:09:32 +0100

    alternative for boxes

--- testshell.sh
+++ testshell.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+box() {
+	if hash boxes 2>/dev/null; then
+		boxes -d unicornthink
+	else
+		echo ==========
+		cat
+	fi
+}
+
 # crude way to list cron commands in /etc/cron.d/test
 grep -v ^# /etc/cron.d/test | grep . | sed 's/^.* root //' | sed 's/\ \#.*//' |
 	while read line
 	do
 		
-		echo "testing: sch -c $line" | boxes -d unicornthink
+		echo "testing: sch -c $line" | box
 		sch -c "$line"
 	done
