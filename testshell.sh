3d1b002a89ff818cbe390d72593075fb04b6a616
Bram Daams <b.daams@science.ru.nl>
2020-01-29 13:55:02 +0100

    improved test script

--- testshell.sh
+++ testshell.sh
@@ -9,11 +9,35 @@ box() {
 	fi
 }
 
+testcron=$(dirname $0)/doc/testcrontab
+cronfile=/etc/cron.d/testcrontab
+
+if [ ! -f $cronfile ]
+then
+	echo could not find $cronfile
+	exit 1
+fi
+
+if [ "$(diff $testcron $cronfile)" != "" ]
+then
+	echo $cronfile differs from $testcron
+	echo to overwrite $cronfile, use:
+	echo
+	echo sudo cp $testcron $cronfile
+	echo
+fi
+
 # crude way to list cron commands in /etc/cron.d/test
-grep -v ^# /etc/cron.d/test | grep -v SHELL | grep . | sed 's/^.* root //' | sed 's/\ \#.*//' |
+grep -v ^# $cronfile |
+	grep -v SHELL |
+	# get rid of empty lines
+	grep . |
+	# strip the schedule and user part
+	sed 's/^.* root //' |
+	# strip optional trailing comment
+	sed 's/\ \#.*//' |
 	while read line
 	do
-		
-		echo "testing: sch -c $line" | box
+		echo "testing: sch -c \"${line}\"" | box
 		sch -c "$line"
 	done
