a578fd191a07c4db342315da6aa9cff63dd6f9e9
Bram Daams <B.Daams@science.ru.nl>
2020-04-02 23:09:08 +0200

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite='tests',
     tests_require=TEST_REQUIREMENTS,
     url='https://gitlab.science.ru.nl/bram/sch',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 )
